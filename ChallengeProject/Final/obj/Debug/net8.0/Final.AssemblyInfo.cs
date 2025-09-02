25733fadb310dca6b64855e43695419a189f8f39
Ikenna Benedict Nwaka <inwaka39@gmail.com>
2025-09-02 10:27:16 +0200

    update

--- ChallengeProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
+++ ChallengeProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8d1e09b3d7c332bcc3496eed11a1905d1d6c8cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b88b6579ee0f045223d3ab4379eb9f52fab9d393")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

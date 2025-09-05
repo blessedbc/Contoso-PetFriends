ae4365b4441873856221a42644cb682cfb1f88bb
Ikenna Benedict Nwaka <inwaka39@gmail.com>
2025-09-05 09:43:23 +0200

    method without repeated code

--- ChallengeProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ ChallengeProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5af2b7a68802f6755d0dbfef3a37031114c3b0fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebb605101bb21d9964c4fe58e8ac8bdd88f45c20")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

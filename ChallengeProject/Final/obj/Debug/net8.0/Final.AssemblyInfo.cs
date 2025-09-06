f32d567a3ff66914c62bfb64dbdb4c3d6593c9cf
Ikenna Benedict Nwaka <inwaka39@gmail.com>
2025-09-06 20:59:38 +0200

    Random Dice

--- ChallengeProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
+++ ChallengeProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fd8659e47d197ed4a94c1d97e2fa69b5d0e051e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71fe046395cbc7411705eaa51f341a05ebf710c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ChallengeProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ ChallengeProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11892e72516e317a9ba1f583a65d4e7ca10c546a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71fe046395cbc7411705eaa51f341a05ebf710c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

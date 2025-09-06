380923319be8a45a5fa360fa383ece6539e75b28
Ikenna Benedict Nwaka <inwaka39@gmail.com>
2025-09-06 17:46:15 +0200

    Return Booleans

--- ChallengeProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ ChallengeProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eea10db0feaab2bb17efaed7cce9823afb74871")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d620a63a7c06d856f82f26d31f3d6b3b3cfd47e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

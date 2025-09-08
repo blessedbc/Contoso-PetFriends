c9e0c4f670d161908f6de2c187fb1392e45544bd
Ikenna Benedict Nwaka <inwaka39@gmail.com>
2025-09-08 21:05:43 +0200

    update

--- ChallengeProject/Starter/Program.cs
+++ ChallengeProject/Starter/Program.cs
@@ -9,7 +9,7 @@ string[] pettingZoo =
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
   RandomizeAnimals();
-// string[,] group = AssignGroup();
+ string[,] group = AssignGroup();
 Console.WriteLine("School A");
 // PrintGroup(group);
 
@@ -27,7 +27,20 @@ void RandomizeAnimals()
         pettingZoo[i] = temp;
     }
 }
-foreach(string animal in pettingZoo) 
+
+
+string[,] AssignGroup(int groups = 6) 
 {
-    Console.WriteLine(animal);
+    string[,] result = new string[groups, pettingZoo.Length/groups];
+    int start = 0;
+
+    for (int i = 0; i < groups; i++) 
+    {
+        for (int j = 0; j < result.GetLength(1); j++) 
+        {
+            result[i,j] = pettingZoo[start++];
+        }
+    }
+
+    return result;
 }
